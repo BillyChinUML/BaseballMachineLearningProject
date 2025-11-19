cbec491380d3d3a7fbf0d6bd9b6b1b59f0b25822
Billy <Billy_Chin@student.uml.edu>
2025-11-19 18:30:39 -0500

    Imported potentially important libraries, and started creating the model.

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, log_loss
+
+if __name__ == "__main__":
+    csv_file = "20250704.csv"
+    csv_data = pd.read_csv(csv_file)
+    X_train = csv_data.drop(columns=['pitch_type'])
+    y_train = csv_data['pitch_type']
+    model = LogisticRegression().fit(X_train, y_train)
