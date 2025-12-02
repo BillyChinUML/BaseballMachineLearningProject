9ca64e97a94144b44ce93a91a7443d02b758c66f
Billy <Billy_Chin@student.uml.edu>
2025-12-02 17:42:38 -0500

    Cleaned up the code.

--- main.py
+++ main.py
@@ -6,30 +6,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 
 if __name__ == "__main__":
-    # csv_train_file = "train.csv"
-    # csv_validation_file = "validate.csv"
     csv_file = "20250704_nonull.csv"
     ignored_columns = ['pitch_type', 'game_date', 'spin_dir', 'spin_rate_deprecated', 'break_angle_deprecated', 'break_length_deprecated', 
                        'tfs_deprecated', 'tfs_zulu_deprecated', 'umpire', 'sv_id', 'player_name', 'des', 'batter', 'pitcher', 'events',
                        'home_team', 'away_team', 'game_year', 'pitch_name']
     
-    # Set up training data.
-    # csv_train_data = pd.read_csv(csv_train_file, encoding_errors='ignore')
-    # X_train = csv_train_data.drop(columns=ignored_columns)
-    # X_train = OneHotEncoder().fit_transform(X_train)
-    # y_train = csv_train_data['pitch_type']
-    
-    # Set up validation data.
-    # csv_validation_data = pd.read_csv(csv_validation_file, encoding_errors='ignore')
-    # X_val = csv_validation_data.drop(columns=ignored_columns)
-    # X_val = OneHotEncoder().fit_transform(X_val)
-    # y_val = csv_validation_data['pitch_type']
-
+    # Set up X and y data using the CSV file.
     csv_data = pd.read_csv(csv_file, encoding_errors='ignore')
     X = csv_data.drop(columns=ignored_columns)
     X = OneHotEncoder().fit_transform(X)
     y = csv_data['pitch_type']
-
+    
+    # Split the X and y data into training data and validation data (80/20).
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
 
     # Train and evaluate model.
