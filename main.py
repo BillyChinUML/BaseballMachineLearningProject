f4bcf0f2ed2699c05b10a51e33dea0d1c319a3a4
Billy <Billy_Chin@student.uml.edu>
2025-12-03 19:18:54 -0500

    Dropped more columns that are either irrelevant or have a lot of missing data.

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, log_loss
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 
 if __name__ == "__main__":
     csv_file = "20250704_nonull.csv"
     ignored_columns = ['pitch_type', 'game_date', 'spin_dir', 'spin_rate_deprecated', 'break_angle_deprecated', 'break_length_deprecated', 
-                       'tfs_deprecated', 'tfs_zulu_deprecated', 'umpire', 'sv_id', 'player_name', 'des', 'batter', 'pitcher', 'events',
-                       'home_team', 'away_team', 'game_year', 'pitch_name']
+                       'tfs_deprecated', 'tfs_zulu_deprecated', 'umpire', 'sv_id', 'player_name', 'des', 'events',
+                       'home_team', 'away_team', 'game_year', 'pitch_name', 'hit_location', 'bb_type', 'on_3b', 'on_2b', 'on_1b', 'hc_x',
+                       'hc_y', 'hit_distance_sc', 'launch_speed', 'launch_angle', 'game_pk', 'fielder_2', 'fielder_3', 'fielder_4',
+                       'fielder_5', 'fielder_6', 'fielder_7', 'fielder_8', 'fielder_9', 'estimated_ba_using_speedangle',
+                       'estimated_woba_using_speedangle', 'woba_value', 'woba_denom', 'babip_value', 'iso_value', 'launch_speed_angle',
+                       'bat_speed', 'swing_length', 'estimated_slg_using_speedangle', 'age_pit_legacy', 'age_bat_legacy', 'attack_angle',
+                       'attack_direction', 'swing_path_tilt', 'intercept_ball_minus_batter_pos_x_inches', 'intercept_ball_minus_batter_pos_y_inches']
     
     # Set up X and y data using the CSV file.
     csv_data = pd.read_csv(csv_file, encoding_errors='ignore')
